4624ec6acaf354272de0aa66337ddabe9bf3a8df
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2018-07-23 12:01:36 -0700

    magic 8 ball

---
+++ Ishita/magic8ball.py
@@ -0,0 +1,16 @@
+import random
+a = int(random.randint(0,2))
+
+print("Welcome to the crystal ball or the magic 8 ball. Ask a question about the future. Find out the result.")
+b = input("Ask your question now.Make sure it is answered by a yes or no. The crystal ball is hazy. It is hard to interpret.")
+
+
+
+if a == 0 :
+    print("\nNo.")
+if a == 1 :
+    print("\nYes.")
+if a == "2":
+    print("\nChild! Destiny awaits you.a")
+
+print("There is your answer. Now that you know, you could try to change it. But remember, some things in your destiny are meant to be. By changing it, you might be fulfiling it. Will you take the risk? ")
