88805fe95a5583c745d658c3c12c276ee91f26f8
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2018-08-25 09:36:16 -0700

    Create foodbotpartofit.py

---
+++ Ishita/foodbotpartofit.py
@@ -0,0 +1,37 @@
+print("Hello I am FoodBot, welcome to Mod Pizza. Please choose your item in each category.")
+
+print("If you would like to choose the option of make your own pizza, then got to the cash counter and say so. If you want classiz pizzas, then you are right on track!")
+
+name= input("What is the name you would like your order placed under?")
+when= int(input("Would like to eat here or would you like your order to-go? Enter 1 for eating here and 2 if you would like your meal to-go:"))
+
+number = int(input("How many pizzas would you like to order?"))
+print("-------Pizza--------------------------------------Pizza --------------------------------Pizza----------------------Pizza-----")
+print("Here are your options.")
+print("Maddy - Red sauce and Mozzerella. Enter 1 if you want it. ")
+print("Mad dog - Red sauce, Mozerella, Pepperoni, Mild sausage, Ground beef. Enter 2. ")
+print("Tristan - Mozerella, Asiago, Mushrooms, Roasted red peppers, Pesto drizzle finish. Enter 3.")
+print("Dominic - White sauce, Fresh chopped basil, Asiago, Mild sausage, Red onion, tomatoes. Enter 4.")
+print("Lucy sunshine - Garlic, Mozerella, Artichoke, Parmesan, Red sauce dollops finish. Enter 5.")
+print("Jasper - Red sauce, Mozzerella, Mushrooms, Spicy sausage. Enter 6.")
+print("Dillion James - Red sauce, Garlic, Fresh chopped Basil Mozzerella, Tomatoes, Asiago. Enter 6.")
+print("Calexico - Red sauce, Mozzerella, Grilled chicken, Jalapenos, Gorgonzola, Hot buffalo sauce finish. Enter 7.")
+print("Caspian - BBQ sauce, Mozarella, Grilled Chicken, Red onion, Gorgonzola, BBQ swirl finish.Enter 8.")
+
+
+snum = int("How many salads will you take?")
+print("---------------------Salads--------------Salads -----------------------Salads----------------Salads------------------------------")
+print("Simple - Mixed greens, Roasted red peppers, Asiagos. Enter 9. ")
+print("Deluxe - Grilled Chicken, Salami, Green Bell Peppers, Black olives, Tomatoes, Parmesan. Enter 10.")
+print("Caesar - Romaine, Tomatoes, Parmesan, Asiago, Croutons. Enter 11.")
+
+
+numstripndip = int(input("How many sides will you take?"))
+print("Garlic strips - Dip into red sauce, Pesto, Ranch, Sri- Rancha.Enter 12.")
+print("Cinnamon Strips - Dip into chocolate, Strawberry, Cinnamon Glaze. Enter 13.")
+print("Dessert( No name Cake) - Velvety cream-filled chocolate. Enter 14.")
+
+drinks = int(input("How many drinks?"))
+print("Beer & Wine. Enter 15.")
+print("Milkshakes - Vanilla, Strawberry, Chocolate.ENter 16.")
+print("Fountain Drinks - Soads, House-made, Iced Teas & Lemonades.Enter 17.")
